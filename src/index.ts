08458484866ca9f1add575454d465715eea9614b
a <b>
2023-10-10 22:26:23 +0200

    Remove serialization

--- src/index.ts
+++ src/index.ts
@@ -3,10 +3,8 @@ import { RLNGFullProof, nullifierInput } from "./rln"
 import { GroupDataProvider } from "./providers/dataProvider"
 import { FileProvider } from "./providers/file"
 import { MemoryProvider, GroupData } from "./providers/memory"
-import { serializeProof, deserializeProof } from "./serialize"
 import { ContractProvider } from "./providers/contractProvider/contractProvider"
 
 export { RLN, RLNGFullProof, 
-        serializeProof, deserializeProof,
         nullifierInput, VerificationResult,
         GroupDataProvider, FileProvider, MemoryProvider, GroupData, ContractProvider }
